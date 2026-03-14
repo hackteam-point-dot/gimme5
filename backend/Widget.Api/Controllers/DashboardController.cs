c205068bd00680a05d3f8eb1c65ec9fb94ea44d4
Andrey Gudov <av.gudov@gmail.com>
2026-03-14 11:29:20 +0200

    controllers + swagger

---
+++ backend/Widget.Api/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Widget.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class DashboardController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult OkResult()
+    {
+        return Ok("ok");
+    }
+}
---
+++ backend/Widget.Api/Controllers/MyProfileController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Widget.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class MyProfileController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult OkResult()
+    {
+        return Ok("ok");
+    }
+}
