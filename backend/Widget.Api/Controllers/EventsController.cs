ca3036fa041da7afd9169bd0fcf73b2ca51b8910
rufoxmax <rufoxmax@gmail.com>
2026-03-14 16:09:26 +0200

    Добавил TaskRepository

--- backend/Widget.Api/Controllers/EventsController.cs
+++ backend/Widget.Api/Controllers/EventsController.cs
@@ -11,18 +11,18 @@ public class EventsController(UserRepository userRepository, TasksRepository tas
     [HttpPost]
     public async Task<IActionResult> PostEvent([FromBody] PostEventApiModel args, [FromQuery] string projectId)
     {
-        if (args.Event == EventType.STORY_DONE && !string.IsNullOrEmpty(args.TaskId))
-        {
-            var subtasks = await tasksRepository.GetSubtasksAsync(args.TaskId);
-            foreach (var subtask in subtasks)
-            {
-                if (subtask.StoryPoints > 0 && !string.IsNullOrEmpty(subtask.AssigneeId))
-                {
-                    var xp = (ulong)subtask.StoryPoints * 10;
-                    await userAchievementRepository.IncrementXpAsync(subtask.AssigneeId, xp);
-                }
-            }
-        }
+        // if (args.Event == EventType.STORY_DONE && !string.IsNullOrEmpty(args.TaskId))
+        // {
+        //     var subtasks = await tasksRepository.GetSubtasksAsync(args.TaskId);
+        //     foreach (var subtask in subtasks)
+        //     {
+        //         if (subtask.StoryPoints > 0 && !string.IsNullOrEmpty(subtask.AssigneeId))
+        //         {
+        //             var xp = (ulong)subtask.StoryPoints * 10;
+        //             await userAchievementRepository.IncrementXpAsync(subtask.AssigneeId, xp);
+        //         }
+        //     }
+        // }
         
         return Ok(args);
     }
