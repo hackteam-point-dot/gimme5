4490d4639fd082c617f295526541312c13862e0b
Andrey Gudov <av.gudov@gmail.com>
2026-03-14 15:16:49 +0200

    user repository

---
+++ backend/Widget.Api/Repositories/UserRepository.cs
@@ -0,0 +1,25 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Driver;
+using Widget.Api.Domain;
+
+namespace Widget.Api.Repositories;
+
+public class UserRepository(IMongoDatabase database)
+{
+    public record Key(string UserId, string ProjectId);
+    public record UserItem(
+        Key Id,
+        ulong Balance,
+        DateTime DateCreated);
+
+    private readonly IMongoCollection<UserItem> _collection =
+        database.GetCollection<UserItem>("Users");
+
+    public async Task<UserItem> CreateAsync(string userId, string projectId, ulong balance, CancellationToken ct = default)
+    {
+        var document = new UserItem(new Key(userId, projectId), balance, DateTime.UtcNow);
+        await _collection.InsertOneAsync(document, cancellationToken: ct);
+        return document;
+    }
+}
