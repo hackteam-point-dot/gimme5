684931eb1126f146edc9811ec0b58491ecd06e4f
rufoxmax <rufoxmax@gmail.com>
2026-03-15 11:26:16 +0200

    Added achivements to leaderboard

--- backend/Widget.Api/Controllers/DashboardController.cs
+++ backend/Widget.Api/Controllers/DashboardController.cs
@@ -6,7 +6,8 @@ namespace Widget.Api.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class DashboardController(UserRepository userRepository) : ControllerBase
+public class DashboardController(UserRepository userRepository, UserAchievementRepository userAchievementRepository)
+    : ControllerBase
 {
     [HttpGet("leaderboard")]
     public async Task<ActionResult<UserLeaderboardApiModel>> GetLeaderboard([FromQuery] int limit = 10,
@@ -16,9 +17,22 @@ public class DashboardController(UserRepository userRepository) : ControllerBase
         var users = await userRepository.GetLeaderboardAsync(limit, skip, ct);
         var totalCount = await userRepository.GetTotalUsersCount(ct);
 
-        var leaderboard =
-            new UserLeaderboardApiModel(users.Select(u => new UserLeaderboardApiModel.Item(u.Id, u.Xp, u.Level)),
-                skip, totalCount);
+        var userIds = users.Select(u => u.Id).ToArray();
+        var userAchievements = await userAchievementRepository.GetByUserIds(userIds, ct);
+        var userAchievementLevelsByUserId = userAchievements
+            .GroupBy(x => x.Key.UserId)
+            .ToDictionary(x => x.Key, x => x.ToDictionary(y => y.Achievement, y => y.Level));
+
+        var items = users.Select(u =>
+        {
+            var achievements = userAchievementLevelsByUserId.TryGetValue(u.Id, out var levels)
+                ? levels.Select(x => Mapper.MapUserAchievementApiModel(x.Key, x.Value)).ToArray()
+                : []
+            ;
+            return new UserLeaderboardApiModel.Item(u.Id, u.Xp, u.Level, achievements);
+        });
+        
+        var leaderboard = new UserLeaderboardApiModel(items, skip, totalCount);
 
         return Ok(leaderboard);
     }
