5f6246ab9ea735b3c80ac54f0491928402fa2fc3
rufoxmax <rufoxmax@gmail.com>
2026-03-15 09:48:41 +0200

    leaderboard

--- backend/Widget.Api/Repositories/UserRepository.cs
+++ backend/Widget.Api/Repositories/UserRepository.cs
@@ -12,30 +12,25 @@ public class UserRepository(IMongoDatabase database)
         int Level,
         DateTime DateCreated);
 
-    public record UserProjectKey(string UserId, string ProjectId);
-
-    [BsonIgnoreExtraElements]
-    public record UserProjectItem(
-        UserProjectKey Id,
-        ulong Balance);
-
     private readonly IMongoCollection<UserItem> _usersCollection = database.GetCollection<UserItem>("Users");
-
-    private readonly IMongoCollection<UserProjectItem> _userProjectsCollection =
-        database.GetCollection<UserProjectItem>("UserProjects");
     
     public async Task<UserItem?> GetUserById(string userId, CancellationToken ct = default)
     {
         return await _usersCollection.Find(x => x.Id == userId).FirstOrDefaultAsync(ct);
     }
 
-    public async Task<List<UserProjectItem>> GetLeaderboardAsync(string projectId, int limit = 10,
-        CancellationToken ct = default)
+    public async Task<List<UserItem>> GetLeaderboardAsync(int limit = 10, int skip = 0, CancellationToken ct = default)
     {
-        return await _userProjectsCollection.Find(u => u.Id.ProjectId == projectId)
-            .SortByDescending(u => u.Balance)
+        return await _usersCollection.Find(u => true)
+            .SortByDescending(u => u.Xp)
+            .Skip(skip)
             .Limit(limit)
             .ToListAsync(cancellationToken: ct);
+    }
+    
+    public async Task<long> GetTotalUsersCount(CancellationToken ct = default)
+    {
+        return await _usersCollection.CountDocumentsAsync(u => true, cancellationToken: ct);
     }
 
     public async Task<UserItem?> SetXpAndLevel(string userId, ulong xp, int level, CancellationToken ct = default)
