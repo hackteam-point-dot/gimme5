0f074665ac03291f480f115b7f5bf57a6b944335
rufoxmax <rufoxmax@gmail.com>
2026-03-15 12:30:22 +0200

    Leaderboard by periods and projects

--- backend/Widget.Api/Application/AchievementService.cs
+++ backend/Widget.Api/Application/AchievementService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Text;
 using Widget.Api.ApiModels;
+using Widget.Api.Domain;
 using Widget.Api.Domain.Targets;
 using Widget.Api.Repositories;
 
@@ -11,21 +12,21 @@ public class AchievementService(
     UserAchievementRepository userAchievementRepository,
     ProjectConfigurationRepository projectConfigurationRepository)
 {
-    public record AchievementResult(string AchievementName, ulong Exp, string[] TaskIds);
+    public record AchievementResult(string AchievementName, ulong Exp, string[] TaskIds, Achievement[] Achievements);
 
     public async Task<AchievementResult> CalculateAchievements(TasksRepository.TaskItem task, PostEventApiModel action,
         string userId)
     {
         if (task.ExpAwarded)
-            return new AchievementResult(string.Empty, 0, []);
-        
+            return new AchievementResult(string.Empty, 0, [], []);
+
         var tasks = await tasksRepository.GetAllAsync();
         var config = await projectConfigurationRepository.GetByProjectIdAsync(action.ProjectKey);
 
-        var awardedAchievements = new List<string>();
+        var awardedAchievements = new List<Achievement>();
         ulong totalExp = 0;
         var taskIds = new List<string>();
-        
+
         foreach (var a in systemAchievements)
         {
             var result = a.Achieve(action, config, tasks);
@@ -33,12 +34,13 @@ public class AchievementService(
             if (result.IsAchieved)
             {
                 await userAchievementRepository.CreateOrUpdateAsync(userId, a.Achievement);
-                awardedAchievements.Add(a.Achievement.ToString());
+                awardedAchievements.Add(a.Achievement);
                 totalExp += result.Exp;
                 taskIds.AddRange(result.TaskIds);
             }
         }
 
-        return new AchievementResult(string.Join(", ", awardedAchievements), totalExp, taskIds.Distinct().ToArray());
+        return new AchievementResult(string.Join(", ", awardedAchievements), totalExp, taskIds.Distinct().ToArray(),
+            awardedAchievements.ToArray());
     }
 }
